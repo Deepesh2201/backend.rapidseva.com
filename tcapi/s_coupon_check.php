e43a267178c6483781df749c7786c2ea8811de9a
Deepesh2201 <115881125+Deepesh2201@users.noreply.github.com>
2023-10-17 00:38:48 +0530

    Update s_coupon_check.php

--- tcapi/s_coupon_check.php
+++ tcapi/s_coupon_check.php
@@ -14,31 +14,30 @@ if ($data['coupon'] == '') {
 
     $chek = $mysqli->query("SELECT * from tbl_coupon where coupon_code = '$coupon'");
     if ($chek->num_rows > 0) {
-        // The coupon exists; now, retrieve its 'id'
         $row = $chek->fetch_assoc();
         $couponLimit = $row['user_limit'];
         $couponId = $row['id'];
         $discPer = $row['disc_per'];
         $maxDisc = $row['max_disc'];
-        // $couponId = $row['id'];
 
-        $count = $mysqli->query("SELECT tc.*, COUNT(tcu.id) AS usage_count
+        $countQuery = $mysqli->query("SELECT COUNT(tcu.id) AS usage_count
         FROM tbl_coupon tc 
         LEFT JOIN tbl_coupon_usage tcu ON tc.id = tcu.coupon_id
         WHERE tc.coupon_code = '$coupon'
         AND tcu.user_id = '$userId'
         GROUP BY tc.id
         ");
-        if ($couponLimit >= $count) {
+        $countRow = $countQuery->fetch_assoc();
+        $usageCount = $countRow['usage_count'];
+
+        if ($usageCount >= $couponLimit) {
             $returnArr = array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Coupon Already Used");
         } else {
-            $returnArr = array("ResponseCode" => "200", "Result" => "true", "ResponseMsg" => "Coupon Found", "CouponID" => $couponId, "CouponLimit" => $couponLimit, "DiscPer" => $discPer, "MaxDisc" => $maxDisc);
+            $returnArr = array("ResponseCode" => "200", "Result" => "true", "ResponseMsg" => "Coupon Found", "CouponID" => $couponId, "CouponLimit" => $couponLimit, "DiscPer" => $discPer, "MaxDisc" => $maxDisc, "UsageCount" => $usageCount);
         }
-
-        // Now, you have the 'id' for the matching coupon code
-
-    } else {
-        $returnArr = array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Coupon Not Found");
+    }
+    else{
+        $returnArr = array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Invalid Coupon Code");
     }
 }
 echo json_encode($returnArr);
