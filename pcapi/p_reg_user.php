e867d9c4066896cf7b3dfe360e30307483448232
Deepesh2201 <115881125+Deepesh2201@users.noreply.github.com>
2023-11-23 19:10:49 +0530

    Update p_reg_user.php

--- pcapi/p_reg_user.php
+++ pcapi/p_reg_user.php
@@ -24,7 +24,7 @@ else
 	 $ifscCode = strip_tags(mysqli_real_escape_string($mysqli,$data['ifscCode']));
 	 $bankName = strip_tags(mysqli_real_escape_string($mysqli,$data['bankName']));
     
-     $returnArr = array("ResponseCode"=>"401","Result"=>"false","ResponseMsg"=>"Mobile Number Already Used!");
+    //  $returnArr = array("ResponseCode"=>"401","Result"=>"false","ResponseMsg"=>"Mobile Number Already Used!");
 
      return false;
      
