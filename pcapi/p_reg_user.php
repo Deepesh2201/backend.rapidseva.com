19ceec60fa7dd0fd54ff53ee89fc5f6763a213fb
Deepesh2201 <115881125+Deepesh2201@users.noreply.github.com>
2023-11-23 22:51:59 +0530

    Update p_reg_user.php

--- pcapi/p_reg_user.php
+++ pcapi/p_reg_user.php
@@ -5,27 +5,22 @@ $data = json_decode(file_get_contents('php://input'), true);
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
 // Function to save an image and get its path
-function saveImageAndGetPath($imageData)
+function saveImageAndGetPath($imageData, $uploadDir)
 {
-    $uploadDir = '/assets'; // Set the path to your upload folder on the server
-
     // Decode the base64-encoded image data
     $imageData = base64_decode($imageData);
 
-    // Extract the file extension from the original file name
-    $originalExtension = pathinfo($imageData, PATHINFO_EXTENSION);
-
-    // Generate a unique filename with the same extension
-    $uniqueFilename = uniqid() . '_' . $imageData;
+    // Generate a unique filename
+    $uniqueFilename = uniqid() . '.jpg'; // You can modify the extension as needed
 
     // Create the full path to save the file
-    $imagePath = $uploadDir . $uniqueFilename . '.' . $originalExtension;
+    $imagePath = $uploadDir . '/' . $uniqueFilename;
 
     // Save the decoded image data to the specified path
     file_put_contents($imagePath, $imageData);
 
     // Return the unique filename with extension to be stored in the database
-    return $uniqueFilename . '.' . $originalExtension;
+    return $uniqueFilename;
 }
 
 
@@ -49,14 +44,19 @@ else
 	 $accountHolder = strip_tags(mysqli_real_escape_string($mysqli,$data['accountHolder']));
 	 $ifscCode = strip_tags(mysqli_real_escape_string($mysqli,$data['ifscCode']));
 	 $bankName = strip_tags(mysqli_real_escape_string($mysqli,$data['bankName']));
-     $aadharFrontImage = strip_tags(mysqli_real_escape_string($mysqli, $data['aadharFrontImage']));
-     $aadharBackImage = strip_tags(mysqli_real_escape_string($mysqli, $data['aadharBackImage']));
-     $panCardImage = strip_tags(mysqli_real_escape_string($mysqli, $data['panCardImage']));
-     $localAddressImage = strip_tags(mysqli_real_escape_string($mysqli, $data['localAddressImage']));
-    // $aadharFrontImage = saveImageAndGetPath($data['aadharFrontImage']);
-    // $aadharBackImage = saveImageAndGetPath($data['aadharBackImage']);
-    // $panCardImage = saveImageAndGetPath($data['panCardImage']);
-    // $localAddressImage = saveImageAndGetPath($data['localAddressImage']);
+    //  $aadharFrontImage = strip_tags(mysqli_real_escape_string($mysqli, $data['aadharFrontImage']));
+    //  $aadharBackImage = strip_tags(mysqli_real_escape_string($mysqli, $data['aadharBackImage']));
+    //  $panCardImage = strip_tags(mysqli_real_escape_string($mysqli, $data['panCardImage']));
+    //  $localAddressImage = strip_tags(mysqli_real_escape_string($mysqli, $data['localAddressImage']));
+    // Define the directory where you want to store the images
+$uploadDir = '/assets/partner/documents/upload/';
+
+// Use the saveImageAndGetPath function
+$aadharFrontImage = saveImageAndGetPath($data['aadharFrontImage'], $uploadDir);
+$aadharBackImage = saveImageAndGetPath($data['aadharBackImage'], $uploadDir);
+$panCardImage = saveImageAndGetPath($data['panCardImage'], $uploadDir);
+$localAddressImage = saveImageAndGetPath($data['localAddressImage'], $uploadDir);
+
 
 
      
