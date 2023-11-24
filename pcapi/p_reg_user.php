d039dd0d81532e7704408bd5f4d1fcc724fd2254
Deepesh2201 <115881125+Deepesh2201@users.noreply.github.com>
2023-11-24 12:04:33 +0530

    Update p_reg_user.php

--- pcapi/p_reg_user.php
+++ pcapi/p_reg_user.php
@@ -50,7 +50,7 @@ if (
         $h = new Common();
         $check = $h->InsertData_Api_Id($field_values, $data_values, $table);
         $c = $mysqli->query("select * from partner where id=".$check."")->fetch_assoc();
-        $returnArr = array("PartnerLogin"=>$c,"ResponseCode"=>"200","Result"=>"true","ResponseMsg"=>"Images uploaded successfully");
+        $returnArr = array("PartnerLogin"=>$c,"ResponseCode"=>"200","Result"=>"true","ResponseMsg"=>"Registration Completed Successfully");
     }
 }
 
